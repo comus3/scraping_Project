d08d1bfb6e6e18652eb0cf3d10985a0028f7c4b1
comus3 <come.plantin.carrenard@gmail.com>
2023-11-13 16:25:06 +0100

    main bug fix

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
-from methods import accessPage,createLinks,findDataValues,findMail
+import os
+import pandas as pd
 import json
 
-def write_dict_to_file(dictionary, filename='output.txt'):
+def read_dict_from_file(filename='output.txt'):
     """
-    Write a dictionary to a text file in JSON format.
+    Read a dictionary from a text file.
 
     Parameters:
-    - dictionary: The dictionary to be written to the file.
-    - filename: The name of the file (default is 'output.txt').
+    - filename: The name of the file to read the dictionary from (default is 'output.txt').
+
+    Returns:
+    - data_dict: The dictionary read from the file.
     """
-    with open(filename, 'w') as file:
-        json.dump(dictionary, file)
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(parent_dir, filename)
 
-# Example usage:
-example_dict = {'key1': ['value1', 'value2'], 'key2': ['value3', 'value4']}
-write_dict_to_file(example_dict)
+    try:
+        with open(file_path, 'r') as file:
+            data_dict = json.load(file)
+        return data_dict
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return None
 
+def save_dict_to_excel(data_dict, excel_filename='output.xlsx'):
+    """
+    Save a dictionary to an Excel file.
 
-def run_fetch_and_save_with_retry(link, max_retries=5):
-    for _ in range(max_retries):
-        result = accessPage.fetch_and_save_html(link)
-        if result == 1:
-            print("Retrying...")
-        elif result == 0:
-            print("Fetching and saving HTML successful.")
-            return findMail.extract_emails_from_html(link)
-        else:
-            print("Error fetching and saving HTML.")
-            break
-    print("Max retries exceeded.")
-    return None
+    Parameters:
+    - data_dict: The dictionary to be saved to the Excel file.
+    - excel_filename: The name of the Excel file (default is 'output.xlsx').
+    """
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    excel_path = os.path.join(parent_dir, excel_filename)
 
-locations = findDataValues.createLocationLinks()
-linkList = createLinks.createLinkList(locations)
+    # Create a DataFrame from the dictionary
+    df = pd.DataFrame(data_dict.items(), columns=['Region', 'Email Addresses'])
 
-output = {}
+    # Write the DataFrame to an Excel file
+    df.to_excel(excel_path, index=False)
 
-index = 0
-for link in linkList:
-    output[locations[index]] = run_fetch_and_save_with_retry(link)
-    
-write_dict_to_file(output)
+    print(f"Data successfully saved to {excel_path}")
+
+# Reading the dictionary from the 'output.txt' file
+loaded_dict = read_dict_from_file()
+
+if loaded_dict:
+    # Saving the dictionary to an Excel file
+    save_dict_to_excel(loaded_dict)
