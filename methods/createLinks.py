23b8d64d61ebeb60f3892935b125bd4645246403
comus3 <come.plantin.carrenard@gmail.com>
2023-12-07 21:01:37 +0100

    create links updated

--- methods/createLinks.py
+++ methods/createLinks.py
@@ -1,6 +1,6 @@
 def createLinkList(locationList):
     X = []
-    linkTemplate = "https://www.ffc.fr/clubs/?fwp_disciplines=route&fwp_club_region={}&fwp_club_activity=route"
+    linkTemplate = "https://velo.ffc.fr/liste-clubs/?discipline=route&activite=cyclisme-pour-tous&region={}"
     for element in locationList:
         X.append(linkTemplate.format(element))
     print(X)
